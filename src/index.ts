416aba40ef8e47cb263d5060eef42a2205b64470
David <olaniteolanight@gmail.com>
2024-04-28 03:26:55 +0100

    Manage restaurant api

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,21 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/myUserRoute";
-
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute"
 
 /**
  * Connects to the MongoDB database using the connection string from the environment variables.
  */
-
 mongoose
 	.connect(process.env.MONGODB_CONNECTION_STRING as string)
 	.then(() => console.log("connected to database!"));
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 
@@ -25,6 +31,7 @@ app.get("/health", async (req: Request, res: Response) => {
 
 // Routes
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 // Start the server
 app.listen(7000, () => {
--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -46,3 +46,25 @@ export const validateMyUserRequest = [
     .isLength({ min: 11, max: 11 }).withMessage("Phone number must be 11 digits"),
 	handleValidationErrors,
 ];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName").notEmpty().withMessage("Restaurant name is required"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Delivery price must be a positive number"),
+  body("estimatedDeliveryTime")
+    .isInt({ min: 0 })
+    .withMessage("Estimated delivery time must be a postivie integar"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be an array")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array cannot be empty"),
+  body("menuItems").isArray().withMessage("Menu items must be an array"),
+  body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu item price is required and must be a postive number"),
+  handleValidationErrors,
+];
